d1757f231f0f00b53b587229c91517417ffad447
Luca <jhlvc4@hotmail.com>
2026-04-18 13:24:08 -0300

    add coverage report

--- vitest.config.js
+++ vitest.config.js
@@ -3,5 +3,13 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     setupFiles: ['./vitest.setup.js'],
+    environment: 'node',
+    coverage: {
+      provider: 'v8', // ou 'istanbul'
+      reporter: ['text', 'json', 'html'], // 'html' gera o relatório visual
+      reportsDirectory: './coverage', // onde o relatório será salvo
+      include: ['usecase/**'], // Foca nos seus Use Cases
+      exclude: ['node_modules/', 'infra/**'], // Ignora infraestrutura
+    },
   },
 });
