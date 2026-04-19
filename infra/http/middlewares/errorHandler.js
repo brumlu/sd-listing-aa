8ae1250379deb3452cb5de95b6480820b9d7f4a2
Brum <97054544+brumlu@users.noreply.github.com>
2026-04-19 14:20:22 -0300

    Atualizar o errorHandler.js

--- infra/http/middlewares/errorHandler.js
+++ infra/http/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 import { AppError } from '../../../model/errors/AppError.js';
 
 export function errorHandler(err, req, res, next) {
-  // Se for um erro que nós criamos (instância de AppError)
+  // Se for um erro já criado (instância de AppError)
   if (err instanceof AppError || err.statusCode) {
     return res.status(err.statusCode || 400).json({
       message: err.message
@@ -9,7 +9,7 @@ export function errorHandler(err, req, res, next) {
   }
 
   // Se cair aqui, é um erro inesperado (Bug ou Banco fora do ar)
-  console.error(' [ERRO NÃO TRATADO]:', err);
+  console.error('ERRO NÃO TRATADO:', err);
 
   return res.status(500).json({
     message: 'Ocorreu um erro interno no servidor.'
